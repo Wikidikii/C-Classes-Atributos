102f0ed02a3f9a7767e0d754b04bed321476083f
Kawê Almeida <68926941+Wikidikii@users.noreply.github.com>
2023-03-10 12:27:40 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno
+{
+    public class Aluno
+    {
+        public string nome;
+        public  int codmat;
+        public string cpf;
+        public char sexo;
+        public double nota_p1;
+        public double nota_p2;
+        public double nota_p3;
+        private double media;
+
+
+        public void ImprimeAluno()
+        {
+            Console.WriteLine(nome);
+            Console.WriteLine(codmat);
+            Console.WriteLine(cpf);
+        }
+        public void ImprimeSexo()
+        {
+            Console.WriteLine(nome + " é");
+            if (sexo == 'M')
+            {
+                Console.WriteLine("Masculino");
+            }
+            if (sexo == 'F')
+            {
+                Console.WriteLine("Feminino");
+            }
+        }
+        public void MediaAluno ()
+        {
+           if (nota_p1 > nota_p2 && nota_p3 > nota_p2)
+            {
+                media = (nota_p1 + nota_p3) / 2;
+            }
+            else if (nota_p1 > nota_p3 && nota_p2 > nota_p3)
+            {
+                media = (nota_p1 + nota_p2) / 2;
+            }
+            else
+            {
+                media= (nota_p2 + nota_p3) / 2;
+            }
+            Console.WriteLine(media);
+        }
+        public void Resultado()
+        {
+            if (media >= 6.0)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
---
+++ TesteAluno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno
+{
+    public class TesteAluno
+    {
+        static void Main(string[] args)
+        {
+            Aluno X1 = new Aluno();
+            X1.nome = "Paulo";
+            X1.codmat = 55123;
+            X1.cpf = "800912345-12";
+            X1.sexo = 'M';
+            X1.nota_p1 = 7.0;
+            X1.nota_p2 = 6.0;
+            X1.nota_p3 = 8.0;
+
+            Aluno X2 = new Aluno();
+            X2.nome = "Ana";
+            X2.codmat = 991239;
+            X2.cpf = "500876123-15";
+            X2.sexo = 'F';
+            X2.nota_p1 = 2.0;
+            X2.nota_p2 = 6.0;
+            X2.nota_p3 = 9.0;
+
+
+            X1.ImprimeAluno();
+            X1.MediaAluno();
+            X1.Resultado();
+            X1.ImprimeSexo();
+            Console.WriteLine("");
+            X2.ImprimeAluno();
+            X2.MediaAluno();
+            X2.Resultado();
+            X2.ImprimeSexo();
+        }
+    }
+}
